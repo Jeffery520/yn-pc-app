d86e64d00bb52ddd5eaf1dc591ad97e0b23a5446
“Jeffery Liang <505691068@qq.com>
2019-10-24 21:04:20 +0800

    格式化代码

--- src/views/alerts/index.vue
+++ src/views/alerts/index.vue
@@ -3,9 +3,7 @@
     <header>
       <div style="width: 600px;">
         <el-input :placeholder="$t('alerts.placeholder')" v-model="search">
-          <template slot="append" style="background:#5F9DE9;">
-            {{ $t("action.search") }}
-          </template>
+          <template slot="append" style="background:#5F9DE9;">{{ $t("action.search") }}</template>
         </el-input>
       </div>
     </header>
@@ -27,60 +25,32 @@
       <el-table-column prop="fMsgContent" show-overflow-tooltip>
         <template slot-scope="scope">
           <!-- 正常提示内容-->
-          <span v-if="scope.row.fAlertType != 1">{{
-            scope.row.fMsgContent
-          }}</span>
+          <span v-if="scope.row.fAlertType != 1">{{scope.row.fMsgContent}}</span>
           <!-- SOS -->
-          <span
-            v-if="scope.row.fAlertType == 1"
-            style="font-size: 18px;color:#E65945;"
-            >{{ scope.row.fMsgContent }}</span
-          >
-          <span
-            v-if="scope.row.fAlertType == 5"
-            style="font-size: 18px;color:#E65945;"
-            >{{ scope.row.fBloodsugar }}</span
-          >
+          <span v-if="scope.row.fAlertType == 1" style="font-size: 18px;color:#E65945;">{{ scope.row.fMsgContent }}</span>
+          <span v-if="scope.row.fAlertType == 5" style="font-size: 18px;color:#E65945;">{{ scope.row.fBloodsugar }}</span>
           <span></span>
           <!--          姓名和日期-->
-          <span>{{
+          <span>
+            {{
             ` - ${scope.row.fFullname || "unknown"} - ${formatTime(
-              scope.row.fAlertTime
+            scope.row.fAlertTime
             )}`
-          }}</span>
+            }}
+          </span>
         </template>
       </el-table-column>
       <el-table-column width="140">
         <template slot-scope="scope">
-          <i
-            slot="reference"
-            @click.stop="showAlertInfo(scope)"
-            class="el-icon-info"
-          ></i>
+          <i slot="reference" @click.stop="showAlertInfo(scope)" class="el-icon-info"></i>
         </template>
       </el-table-column>
       <el-table-column prop="fAlertStaus" width="140">
         <template slot-scope="scope">
-          <span
-            v-if="scope.row.fAlertStaus == 1"
-            style="font-size: 20px;color:#E65945;"
-            >Open</span
-          >
-          <span
-            v-if="scope.row.fAlertStaus == 2"
-            style="font-size: 20px;color:#E65945;"
-            >Skip</span
-          >
-          <span
-            v-if="scope.row.fAlertStaus == 3"
-            style="font-size: 20px;color:#38CB73;"
-            >Follow up</span
-          >
-          <span
-            v-if="scope.row.fAlertStaus == 4"
-            style="font-size: 20px;color:#629EE7;"
-            >Completed</span
-          >
+          <span v-if="scope.row.fAlertStaus == 1" style="font-size: 20px;color:#E65945;">Open</span>
+          <span v-if="scope.row.fAlertStaus == 2" style="font-size: 20px;color:#E65945;">Skip</span>
+          <span v-if="scope.row.fAlertStaus == 3" style="font-size: 20px;color:#38CB73;">Follow up</span>
+          <span v-if="scope.row.fAlertStaus == 4" style="font-size: 20px;color:#629EE7;">Completed</span>
         </template>
       </el-table-column>
       <el-table-column width="60" align="left">
@@ -90,87 +60,77 @@
       </el-table-column>
     </el-table>
     <!--分页组件-->
-    <Pagination
-      ref="Pagination"
-      :currentPage="currentPage"
-      @currentChange="pageChange"
-    ></Pagination>
+    <Pagination ref="Pagination" :currentPage="currentPage" @currentChange="pageChange"></Pagination>
     <!--简要Info弹窗-->
-    <alertInfo
-      ref="alertInfo"
-      @openDetail="openDetail"
-      :dataInfo="currentInfo"
-    ></alertInfo>
+    <alertInfo ref="alertInfo" @openDetail="openDetail" :dataInfo="currentInfo"></alertInfo>
     <!--Detail弹窗-->
     <alertDetail ref="alertDetail" :detail="currentDetail"></alertDetail>
   </div>
 </template>
 
 <script>
-import mixin from "@/views/mixin";
-import { formatDate } from "@/utils/validate";
-import { getCsdn } from "@/api/user";
-import { getAlertList } from "@/api/alert";
-import Pagination from "@/components/Pagination/index.vue";
-import alertInfo from "@/components/Alerts/alertInfo.vue";
-import alertDetail from "@/components/Alerts/alertDetail.vue";
-export default {
-  name: "Alerts",
-  mixins: [mixin],
-  components: { Pagination, alertInfo, alertDetail },
-  data() {
-    return {
-      tableData: [],
-      search: "",
-      currentPage: 1,
-      currentInfo: {},
-      currentDetail: {}
-    };
-  },
-  mounted() {
-    getAlertList()
-      .then(response => {
-        let { list } = response;
-        console.log(response);
-        this.tableData = list;
-      })
-      .catch(error => {
-        console.log(error);
+  import mixin from "@/views/mixin";
+  import { formatDate } from "@/utils/validate";
+  import { getCsdn } from "@/api/user";
+  import { getAlertList } from "@/api/alert";
+  import Pagination from "@/components/Pagination/index.vue";
+  import alertInfo from "@/components/Alerts/alertInfo.vue";
+  import alertDetail from "@/components/Alerts/alertDetail.vue";
+  export default {
+    name: "Alerts",
+    mixins: [mixin],
+    components: { Pagination, alertInfo, alertDetail },
+    data() {
+      return {
+        tableData: [],
+        search: "",
+        currentPage: 1,
+        currentInfo: {},
+        currentDetail: {}
+      };
+    },
+    mounted() {
+      getAlertList()
+        .then(response => {
+          let { list } = response;
+          console.log(response);
+          this.tableData = list;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      getCsdn().then(function(response) {
+        console.log("getCsdn调试请求成功===============alert.vue");
       });
-    getCsdn().then(function(response) {
-      console.log("getCsdn调试请求成功===============alert.vue");
-    });
-  },
-  methods: {
-    showAlertInfo({ row }) {
-      this.$refs.alertInfo.infoVisible = true;
-      this.currentInfo = row;
     },
-    showDetailInfo(row) {
-      console.log(row,       '显示detail弹窗');
-      // 显示detail弹窗
-      this.$refs.alertDetail.detailVisible = true;
-      this.currentDetail = row;
-    },
-    // 通过AlertInfo组件触发
-    openDetail() {
-      this.showDetailInfo(this.currentInfo);
-      console.log("通过AlertInfo组件触发");
-    },
-    pageChange(ev) {
-      console.log(ev);
-      this.$refs.Pagination.currentPage = ev;
-    },
-    formatTime(timestamp) {
-      const dateObj = formatDate(timestamp, "en");
-      return `${dateObj.month} ${dateObj.day} - ${
-        dateObj.hour < 10 ? "0" + dateObj.hour : dateObj.hour
-      }:${dateObj.minute < 10 ? "0" + dateObj.minute : dateObj.minute} ${
-        dateObj.ampm
-      } `;
+    methods: {
+      showAlertInfo({ row }) {
+        this.$refs.alertInfo.infoVisible = true;
+        this.currentInfo = row;
+      },
+      showDetailInfo(row) {
+        console.log(row, "显示detail弹窗");
+        // 显示detail弹窗
+        this.$refs.alertDetail.detailVisible = true;
+        this.currentDetail = row;
+      },
+      // 通过AlertInfo组件触发
+      openDetail() {
+        this.showDetailInfo(this.currentInfo);
+        console.log("通过AlertInfo组件触发");
+      },
+      pageChange(ev) {
+        console.log(ev);
+        this.$refs.Pagination.currentPage = ev;
+      },
+      formatTime(timestamp) {
+        const dateObj = formatDate(timestamp, "en");
+        return `${dateObj.month} ${dateObj.day} - ${dateObj.hour < 10 ? "0" + dateObj.hour : dateObj.hour}:${
+          dateObj.minute < 10 ? "0" + dateObj.minute : dateObj.minute
+        } ${dateObj.ampm} `;
+      }
     }
-  }
-};
+  };
 </script>
 <style lang="scss">
 @import "@/style/mixin.scss";
