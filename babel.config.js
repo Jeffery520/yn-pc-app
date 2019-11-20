dae8bce4febdd6978315cd45b30223e66cf1f1fb
JefferyLiang <liangxingyu@jingdaka.com>
2019-11-21 00:19:50 +0800

    echarts hr

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 // remove console
-const plugins = [];
+const plugins = [
+	[
+		'component',
+		{
+			libraryName: 'element-ui',
+			styleLibraryName: 'theme-chalk'
+		},
+		'syntax-dynamic-import'
+	]
+];
 if (IS_PROD) {
 	plugins.push('transform-remove-console');
 }
